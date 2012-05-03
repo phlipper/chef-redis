238fa708413003c321daea3233d1d256567bb788
Phil Cohen <github@phlippers.net>
2012-05-02 17:11:10 -0700

    doesn't depend on build-essential

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer        "Phil Cohen"
 maintainer_email  "github@phlippers.net"
 license           "MIT"
 description       "Installs and configures Redis"
-version           "0.1.0"
+version           "0.1.1"
 
 recipe "redis", "Sets up ppa apt repository"
 recipe "redis::server", "Installs redis server"
@@ -11,5 +11,3 @@ recipe "redis::doc", "Installs redis documentation"
 %w{ ubuntu debian }.each do |os|
   supports os
 end
-
-depends "build-essential"

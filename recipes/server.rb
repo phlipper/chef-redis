cab09bfa28d9ee26e548d48e27fbaabec5dd703c
Phil Cohen <github@phlippers.net>
2012-05-02 17:50:30 -0700

    write server config template and support service management

--- recipes/server.rb
+++ recipes/server.rb
@@ -6,3 +6,17 @@
 include_recipe "redis::default"
 
 package "redis-server"
+
+service "redis-server" do
+  supports :restart => true
+  action [ :enable, :start ]
+end
+
+template "/etc/redis/redis.conf" do
+  source "redis.conf.erb"
+  owner  "root"
+  group  "root"
+  mode   "0644"
+  action :create
+  notifies :restart, "service[redis-server]"
+end

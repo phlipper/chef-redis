89718360f3f953295da4b77a4b36ab4e48be6f19
Phil Cohen <github@phlippers.net>
2014-01-15 11:03:04 -0800

    bump version to 0.5.3

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Phil Cohen"
 maintainer_email  "github@phlippers.net"
 license           "MIT"
 description       "Installs and configures Redis"
-version           "0.5.2"
+version           "0.5.3"
 
 recipe "redis::default", "Sets up ppa apt repository"
 recipe "redis::server", "Installs redis server"

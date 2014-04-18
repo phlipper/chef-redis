d700185bd7d2787b37f8bb0246feed017c522088
Gabriel Mazetto <brodock@gmail.com>
2014-04-18 19:41:51 -0300

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Phil Cohen"
 maintainer_email  "github@phlippers.net"
 license           "MIT"
 description       "Installs and configures Redis"
-version           "0.5.4"
+version           "0.5.5"
 
 recipe "redis::default", "Sets up ppa apt repository"
 recipe "redis::server", "Installs redis server"
